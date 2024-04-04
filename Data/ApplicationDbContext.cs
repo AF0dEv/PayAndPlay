e9851eb562bd340490ed075b2e62f18c69b349a7
AF0dEv <afonsopro.business@gmail.com>
2024-04-04 10:18:38 +0100

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PayAndPlay.Models;
+
+namespace PayAndPlay.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
